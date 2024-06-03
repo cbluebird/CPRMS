7b55e75f603f490741a6a77ef7529350f3e1aa6b
XiMo-210 <2831802697@qq.com>
2024-06-03 22:54:55 +0800

    fix: 修复AuthFilter鉴权问题

--- src/main/java/team/sugarsmile/cprms/filter/AuthFilter.java
+++ src/main/java/team/sugarsmile/cprms/filter/AuthFilter.java
@@ -4,45 +4,41 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
 import team.sugarsmile.cprms.exception.BizException;
 import team.sugarsmile.cprms.model.Admin;
 import team.sugarsmile.cprms.service.AdminService;
 import team.sugarsmile.cprms.service.RoleService;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 
-@Slf4j
-@WebFilter("/admi/*")
+@WebFilter("/admin/*")
 public class AuthFilter implements Filter {
-
     private final RoleService roleService = new RoleService();
     private final AdminService adminService = new AdminService();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpSession session = request.getSession();
         try {
-            Admin admin = (Admin) session.getAttribute("admin");
-            String uri = request.getRequestURI();
-            Admin adminCheck = adminService.getAdminByID(admin.getId());
-            roleService.checkPermission(uri, adminCheck);
+            Admin admin = (Admin) request.getSession().getAttribute("admin");
+            String path = request.getServletPath();
+            adminService.getAdminByID(admin.getId());
+            roleService.checkPermission(path, admin);
+            filterChain.doFilter(servletRequest, servletResponse);
         } catch (BizException e) {
-            response.sendRedirect("/login.jsp");
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
         }
     }
 
     @Override
     public void destroy() {
-
+        Filter.super.destroy();
     }
 }
 
